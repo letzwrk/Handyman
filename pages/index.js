b6384f6f1c45c3842a7e76a8f8142980c5c46ebf
running water never stales <91630063+letzwrk@users.noreply.github.com>
2023-02-14 21:53:48 -0600

    kys

--- pages/index.js
+++ pages/index.js
@@ -21,12 +21,6 @@ export default function Home() {
           </p>
 
         </div>
-<<<<<<< HEAD
-        
-<Footer>
-=======
-
-
 
         <div className={styles.grid}>
           <a
@@ -77,7 +71,8 @@ export default function Home() {
             </p>
           </a>
         </div>
->>>>>>> 0625957193edf3fd6cbc1a6cf8530193979dd520
+        
+<Footer>
         <div className={styles.center}>
           <Image
             className={styles.logo}
@@ -97,69 +92,6 @@ export default function Home() {
             />
           </div>
         </div>
-<<<<<<< HEAD
-</Footer>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              electrical 
-              <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-=======
         <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -177,7 +109,8 @@ export default function Home() {
               />
             </a>
           </div>
->>>>>>> 0625957193edf3fd6cbc1a6cf8530193979dd520
+
+          </Footer>
       </main>
     </>
   )
